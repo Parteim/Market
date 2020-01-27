ee111b36af0d1e9b7a33fe32d4e101229e69e5e4
Parteim <Sergej_kajnov@mail.ru>
2020-01-27 09:16:18 +0700

    update cart

--- AtomMarket/cart/views.py
+++ AtomMarket/cart/views.py
@@ -5,12 +5,16 @@ from .models import Cart
 
 def cart(request):
     if request.method == 'POST':
-        product_id = request.POST.get('product')
 
-        product_obj = Cart.objects.get(pk=product_id)
+        try:
+            product_id = request.POST.get('product')
 
-        print(f'=========================={product_obj}=======================')
-        product_obj.delete()
+            product_obj = Cart.objects.get(pk=product_id)
+
+            print(f'=========================={product_obj}=======================')
+            product_obj.delete()
+        except:
+            pass
 
     data = {
         'title': 'ATOM cart',
---
+++ AtomMarket/main/static/js/cart.js
@@ -0,0 +1,15 @@
+function sum_of_price(){
+  let list_of_product = document.querySelectorAll('.cart_product_price');
+  let sum_of_price = document.getElementById('sum_of_price')
+  let sum = 0;
+
+  list_of_product.forEach((item, index) => {
+    sum += parseInt((item.children[0].innerText))
+    console.log(sum);
+  });
+
+  sum_of_price.innerText = sum;
+
+}
+
+sum_of_price()
