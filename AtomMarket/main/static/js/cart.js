cd3983fdda8eebaaf9fa02c741df03a30a6c6f05
Parteim <Sergej_kajnov@mail.ru>
2020-01-27 11:09:18 +0700

    counter for cart

--- AtomMarket/main/static/js/cart.js
+++ AtomMarket/main/static/js/cart.js
@@ -1,6 +1,6 @@
 function sum_of_price(){
   let list_of_product = document.querySelectorAll('.cart_product_price');
-  let sum_of_price = document.getElementById('sum_of_price')
+  let sum_of_price = document.getElementById('sum_of_price');
   let sum = 0;
 
   list_of_product.forEach((item, index) => {
@@ -12,4 +12,4 @@ function sum_of_price(){
 
 }
 
-sum_of_price()
+sum_of_price();
--- AtomMarket/main/static/js/main.js
+++ AtomMarket/main/static/js/main.js
@@ -1,1 +1,4 @@
-
+function count_cart(){
+  let counter = document.getElementById('count_of_product_cart');
+  
+}
