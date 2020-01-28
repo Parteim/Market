c83f4fec63da61905b54e5c426799c0abf2553f7
Parteim <Sergej_kajnov@mail.ru>
2020-01-28 15:58:08 +0300

    Main page Update, created filters for all products

--- AtomMarket/main/urls.py
+++ AtomMarket/main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pc-page/', views.pc_page, name='pc-page'),
+    path('tv-page/', views.tv_page, name='tv-page'),
+    path('mobile-phone-page/', views.mobile_phone_page, name='mobile-phone-page'),
+    path('location-page/', views.location_page, name='location-page'),
 ]
--- AtomMarket/main/views.py
+++ AtomMarket/main/views.py
@@ -20,3 +20,62 @@ def home(request):
     }
 
     return render(request, 'main/index.html', data)
+
+
+def pc_page(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product')
+        product_object = Product.objects.get(name=product_name)
+        print(f'================={type(product_object)}===========')
+        product = Cart(user=request.user, product=product_object)
+        product.save()
+
+    products = Product.objects.all().filter(type_of_product='PC')
+    data = {
+        'title': 'ATOM',
+        'products': products,
+    }
+
+    return render(request, 'main/index.html', data)
+
+
+def tv_page(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product')
+        product_object = Product.objects.get(name=product_name)
+        print(f'================={type(product_object)}===========')
+        product = Cart(user=request.user, product=product_object)
+        product.save()
+
+    products = Product.objects.all().filter(type_of_product='TV')
+    data = {
+        'title': 'ATOM',
+        'products': products,
+    }
+
+    return render(request, 'main/index.html', data)
+
+
+def mobile_phone_page(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product')
+        product_object = Product.objects.get(name=product_name)
+        print(f'================={type(product_object)}===========')
+        product = Cart(user=request.user, product=product_object)
+        product.save()
+
+    products = Product.objects.all().filter(type_of_product='MobilePhone')
+    data = {
+        'title': 'ATOM',
+        'products': products,
+    }
+
+    return render(request, 'main/index.html', data)
+
+
+def location_page(request):
+    data = {
+        'title': 'Location',
+    }
+
+    return render(request, 'main/location.html', data)
