0fed0006bc07b45a25d02ee75fd6b8f1f6914921
Parteim <Sergej_kajnov@mail.ru>
2020-01-30 16:46:46 +0300

    Create search by products. Update index page.

--- AtomMarket/main/urls.py
+++ AtomMarket/main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('tv-page/', views.tv_page, name='tv-page'),
     path('mobile-phone-page/', views.mobile_phone_page, name='mobile-phone-page'),
     path('location-page/', views.location_page, name='location-page'),
+    path('search/', views.search, name='search'),
+    path('add-in-cart/', views.add_in_cart, name='add-in-cart'),
 ]
--- AtomMarket/main/views.py
+++ AtomMarket/main/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView, ListView
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+
 
 from cart.models import Cart
 from products.models import Product
 
 
 def home(request):
+    products = Product.objects.all()
+    data = {
+        'title': 'ATOM',
+        'products': products,
+    }
+
     if request.method == 'POST':
         product_name = request.POST.get('product')
         product_object = Product.objects.get(name=product_name)
         print(f'================={type(product_object)}===========')
         product = Cart(user=request.user, product=product_object)
         product.save()
-
-    products = Product.objects.all()
-    data = {
-        'title': 'ATOM',
-        'products': products,
-    }
 
     return render(request, 'main/index.html', data)
 
@@ -79,3 +82,25 @@ def location_page(request):
     }
 
     return render(request, 'main/location.html', data)
+
+
+def search(request):
+    search_value = request.GET.get('search')
+    products = Product.objects.filter(Q(name__icontains=search_value))
+
+    data = {
+        'title': 'ATOM',
+        'products': products,
+    }
+
+    return render(request, 'main/index.html', data)
+
+
+def add_in_cart(request):
+    product_name = request.POST.get('product')
+    product_object = Product.objects.get(name=product_name)
+    print(f'================={type(product_object)}===========')
+    product = Cart(user=request.user, product=product_object)
+    product.save()
+
+    return redirect('')
